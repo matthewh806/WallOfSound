729ee2f8d0a4d10f26f566918559d3735f941d28
matthewh806 <matthewh806@gmail.com>
2018-09-11 22:52:32 +0200

    saw saw saw saw

--- WallOfSound/main.cpp
+++ WallOfSound/main.cpp
@@ -9,24 +9,48 @@
 #include <iostream>
 #include "RtAudio.h"
 
+int saw(void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
+         double streamTime, RtAudioStreamStatus status, void *userData) {
+    
+    unsigned int i, j;
+    double *buffer = (double *) outputBuffer;
+    double *lastValues = (double *) userData;
+    
+    if(status)
+        std::cout << "Stream underflow detected!" << std::endl;
+    
+    for( i = 0; i<nBufferFrames; i++) {
+        for( j = 0; j < 2; j++) {
+            *buffer++ = lastValues[j];
+            
+            lastValues[j] += 0.005 * (j+1+(j*0.1));
+            if(lastValues[j] >= 1.0) lastValues[j] -= 2.0;
+        }
+    }
+    
+    return 0;
+}
+
 int main(int argc, const char * argv[]) {
-    std::cout << "Hello main!" << std::endl;
     
     RtAudio dac(RtAudio::MACOSX_CORE);
-    if(dac.getDeviceCount() == 0) exit(0);
+    if(dac.getDeviceCount() < 1) {
+        std::cout << "\nNo audio devices found!\n";
+        exit(0);
+    }
     
     RtAudio::StreamParameters parameters;
     parameters.deviceId = dac.getDefaultOutputDevice();
     parameters.nChannels = 2;
+    parameters.firstChannel = 0;
     unsigned int sampleRate = 44100;
     unsigned int bufferFrames = 265;
-    
-    RtAudio::StreamOptions options;
-    options.flags = RTAUDIO_NONINTERLEAVED;
+    double data[2];
     
     try {
         dac.openStream(&parameters, NULL, RTAUDIO_FLOAT32,
-                       sampleRate, &bufferFrames, nullptr, NULL, &options);
+                       sampleRate, &bufferFrames, &saw, (void *)&data);
+        dac.startStream();
     } catch( RtAudioError& e) {
         std::cout << '\n' << e.getMessage() << '\n' << std::endl;
         exit( 0 );
@@ -36,7 +60,6 @@ int main(int argc, const char * argv[]) {
     
     std::string status = dac.isStreamOpen() ? "Open" : "Closed";
     std::cout << "\nStream is open... press <enter> to quit.\n";
-    std::cout << "Stream is: " + status << std::endl;
     std::cin.get(input);
     
     try {
