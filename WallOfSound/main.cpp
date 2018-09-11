207dc04babce4cc8c3889d1abfb792002ce4d08c
matthewh806 <matthewh806@gmail.com>
2018-09-11 19:46:26 +0200

    first files

---
+++ WallOfSound/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WallOfSound
+//
+//  Created by Matthew on 11/09/2018.
+//  Copyright © 2018 Matthew. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
