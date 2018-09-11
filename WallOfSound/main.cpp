e69e14cf4d1299fbbf71b33854b77c269aa2730b
matthewh806 <matthewh806@gmail.com>
2018-09-11 22:25:00 +0200

    Connect to output (broken xcode proj...)

--- WallOfSound/main.cpp
+++ WallOfSound/main.cpp
@@ -7,9 +7,45 @@
 //
 
 #include <iostream>
+#include "RtAudio.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello main!" << std::endl;
+    
+    RtAudio dac(RtAudio::MACOSX_CORE);
+    if(dac.getDeviceCount() == 0) exit(0);
+    
+    RtAudio::StreamParameters parameters;
+    parameters.deviceId = dac.getDefaultOutputDevice();
+    parameters.nChannels = 2;
+    unsigned int sampleRate = 44100;
+    unsigned int bufferFrames = 265;
+    
+    RtAudio::StreamOptions options;
+    options.flags = RTAUDIO_NONINTERLEAVED;
+    
+    try {
+        dac.openStream(&parameters, NULL, RTAUDIO_FLOAT32,
+                       sampleRate, &bufferFrames, nullptr, NULL, &options);
+    } catch( RtAudioError& e) {
+        std::cout << '\n' << e.getMessage() << '\n' << std::endl;
+        exit( 0 );
+    }
+    
+    char input;
+    
+    std::string status = dac.isStreamOpen() ? "Open" : "Closed";
+    std::cout << "\nStream is open... press <enter> to quit.\n";
+    std::cout << "Stream is: " + status << std::endl;
+    std::cin.get(input);
+    
+    try {
+        dac.stopStream();
+    } catch( RtAudioError& e) {
+        e.printMessage();
+    }
+    
+    if ( dac.isStreamOpen() ) dac.closeStream();
+    
     return 0;
 }
