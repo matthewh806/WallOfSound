89ccf11fd56e4480ad55961493d491abd3067406
matthewh806 <matthewh806@gmail.com>
2018-09-15 15:27:07 +0200

    Most basic sinewave wavetable impl

--- WallOfSound/main.cpp
+++ WallOfSound/main.cpp
@@ -7,9 +7,79 @@
 //
 
 #include <iostream>
+#include "math.h"
+#include <vector>
 #include "RtAudio.h"
 
-int saw(void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
+//==============================================================================
+
+class audioSettings {
+    
+public:
+    static int sampleRate;
+    static int channels;
+    static int bufferSize;
+};
+
+//==============================================================================
+
+// TODO: Separate class!
+
+template< int TABLE_SIZE >
+
+class osc {
+public:
+    double frequency;
+    double phase;
+    
+    osc() {
+        phase = 0.0;
+        bufferSize = TABLE_SIZE;
+        
+        auto angleDelta = M_PI_2 / (bufferSize - 1);
+        auto currentAngle = 0.0;
+        
+        for(int i = 0; i < bufferSize; i++) {
+            sineBuffer[i] = sin(currentAngle);
+            currentAngle += angleDelta;
+        }
+    }
+    
+    double sineWave(double frequency)
+    {
+        // phase index = mod_L ( previous_phase + increment) L = length
+        // output = amplitude * wavetable[phase_index]
+        
+        auto output = 0.25 * sineBuffer[(int)phase];
+        
+        increment = (bufferSize * frequency) / 44100;
+        phase = fmod(phase + increment, bufferSize);
+        
+        return output;
+    }
+    
+private:
+    int bufferSize;
+    float increment;
+    
+    float sineBuffer[TABLE_SIZE];
+};
+
+
+//==============================================================================
+
+osc<1024> myOsc;
+
+// TODO: Separate class !
+void play(double *output)
+{
+    
+    // TODO: Play!
+    output[0] = myOsc.sineWave(440);
+    output[1] = output[0];
+}
+
+int routing(void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
          double streamTime, RtAudioStreamStatus status, void *userData) {
     
     unsigned int i, j;
@@ -20,18 +90,22 @@ int saw(void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
         std::cout << "Stream underflow detected!" << std::endl;
     
     for( i = 0; i<nBufferFrames; i++) {
+        play(lastValues);
         for( j = 0; j < 2; j++) {
-            *buffer++ = lastValues[j];
-            
-            lastValues[j] += 0.005 * (j+1+(j*0.1));
-            if(lastValues[j] >= 1.0) lastValues[j] -= 2.0;
+            *buffer++=lastValues[j];
         }
     }
     
     return 0;
 }
 
+//==============================================================================
+
 int main(int argc, const char * argv[]) {
+    
+//    audioSettings::sampleRate = 44100;
+//    audioSettings::channels = 2;
+//    audioSettings::bufferSize = 1024;
     
     RtAudio dac(RtAudio::MACOSX_CORE);
     if(dac.getDeviceCount() < 1) {
@@ -45,11 +119,11 @@ int main(int argc, const char * argv[]) {
     parameters.firstChannel = 0;
     unsigned int sampleRate = 44100;
     unsigned int bufferFrames = 265;
-    double data[2];
+    std::vector<double> data(2,0);
     
     try {
-        dac.openStream(&parameters, NULL, RTAUDIO_FLOAT32,
-                       sampleRate, &bufferFrames, &saw, (void *)&data);
+        dac.openStream( &parameters, NULL, RTAUDIO_FLOAT64,
+                       sampleRate, &bufferFrames, &routing, (void *)&(data[0]));
         dac.startStream();
     } catch( RtAudioError& e) {
         std::cout << '\n' << e.getMessage() << '\n' << std::endl;
@@ -72,3 +146,5 @@ int main(int argc, const char * argv[]) {
     
     return 0;
 }
+
+//==============================================================================
